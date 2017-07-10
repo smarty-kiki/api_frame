968c35e1d28b4db63664f988e6c5588470c93924
smarty-kiki <kiki@smarty.so>
2017-07-10 18:23:21 +0800

    整理说明文件

--- controller/index.php
+++ controller/index.php
@@ -2,20 +2,5 @@
 
 if_get('/', function ()
 {
-    $time = microtime(true);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    queue_push('test', ['test' => 'haha']);
-    echo microtime(true) - $time;
-
-    return 'ok';
+    return 'hello world';
 });
--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 
 sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name micro_service \
     -v $SCRIPT_DIR:/var/www/ms_service \
-    -v $SCRIPT_DIR/project/config/development/nginx/micro_service.conf:/etc/nginx/sites-enabled/micro_service.conf \
+    -v $SCRIPT_DIR/project/config/development/nginx/micro_api.conf:/etc/nginx/sites-enabled/micro_api.conf \
 debian-php-server start
