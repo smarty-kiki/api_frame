c02ed80c441135a59aca6377dbfb04ec6e45989d
smarty-kiki <kiki@smarty.so>
2017-07-11 21:45:34 +0800

    强迫症调整路径名字

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 
 sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name micro_api \
-    -v $SCRIPT_DIR:/var/www/ms_api \
+    -v $SCRIPT_DIR:/var/www/micro_api \
     -v $SCRIPT_DIR/project/config/development/nginx/micro_api.conf:/etc/nginx/sites-enabled/default \
     -v $SCRIPT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
 kikiyao/debian_php_dev_env start
