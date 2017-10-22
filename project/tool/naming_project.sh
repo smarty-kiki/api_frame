c009ca86b4624dcc667cd40da252f0425dbcc358
smarty-kiki <kiki@smarty.so>
2017-10-23 00:01:49 +0800

    修改项目重命名脚本为 api_frame

--- project/tool/naming_project.sh
+++ project/tool/naming_project.sh
@@ -2,7 +2,7 @@
 
 function sed_name
 {
-    cat $1 | sed -e "s/mvc_frame/$2/g" > $1.new && mv $1.new $1
+    cat $1 | sed -e "s/api_frame/$2/g" > $1.new && mv $1.new $1
 }
 
 if [ ! -n "$1" ] ;then
@@ -12,8 +12,8 @@ fi
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 
-mv $ROOT_DIR/project/config/development/nginx/mvc_frame.conf $ROOT_DIR/project/config/development/nginx/$1.conf
-mv $ROOT_DIR/project/config/production/nginx/mvc_frame.conf $ROOT_DIR/project/config/production/nginx/$1.conf
+mv $ROOT_DIR/project/config/development/nginx/api_frame.conf $ROOT_DIR/project/config/development/nginx/$1.conf
+mv $ROOT_DIR/project/config/production/nginx/api_frame.conf $ROOT_DIR/project/config/production/nginx/$1.conf
 
 sed_name $ROOT_DIR/project/config/development/nginx/hrm.conf $1
 sed_name $ROOT_DIR/project/config/development/supervisor/queue_worker.conf $1
