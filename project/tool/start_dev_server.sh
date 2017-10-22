d26f5abec4e7cb6e550f5ff278f6597001cf8506
smarty-kiki <kiki@smarty.so>
2017-10-22 23:38:29 +0800

    更改 micro_api 为 api_frame

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 
-sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name micro_api \
-    -v $SCRIPT_DIR:/var/www/micro_api \
-    -v $SCRIPT_DIR/project/config/development/nginx/micro_api.conf:/etc/nginx/sites-enabled/default \
+sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name api_frame \
+    -v $SCRIPT_DIR:/var/www/api_frame \
+    -v $SCRIPT_DIR/project/config/development/nginx/api_frame.conf:/etc/nginx/sites-enabled/default \
     -v $SCRIPT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
 kikiyao/debian_php_dev_env start
