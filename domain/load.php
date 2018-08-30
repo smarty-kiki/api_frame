20ab6d8acca54496a6695e9a3da0f565606d92da
kiki <kiki@smarty.so>
2018-08-31 01:35:46 +0800

    更新 load 加载逻辑

--- domain/load.php
+++ domain/load.php
@@ -1,11 +1,3 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-
-    $class_maps = array(
-    );
-
-    if (isset($class_maps[$class_name])) {
-         include __DIR__.'/'.$class_maps[$class_name];
-    }
-});
+include __DIR__.'/autoload.php';
