30fbf977fab62fb309d6ae142513c14d6a626355
yaoyang <kiki@smarty.so>
2018-10-12 13:48:31 +0800

    通过领域 yml 生成 restful controller

--- command/controller.php
+++ command/controller.php
@@ -8,11 +8,19 @@ function _generate_controller_file($table, $entity_structs, $entity_relationship
     $list_str = [];
     $input_str = [];
     foreach ($entity_structs as $struct) {
-        $list_str[] = "\$inputs['$struct']";
-        $input_str[] = "'$struct'";
+        $struct_name = $struct['name'];
+
+        $list_str[] = "\$inputs['$struct_name']";
+        $input_str[] = "'$struct_name'";
     }
 
-    $input_content = "\$inputs = [];\n    list(".implode(", ", $list_str).") = input_list(".implode(", ", $input_str).");\n    \$inputs = array_filter(\$inputs);";
+    $input_content = "\$inputs = [];
+    list(
+        ".implode(",\n        ", $list_str)."
+    ) = input_list(
+        ".implode(",\n        ", $input_str)."
+    );
+    \$inputs = array_filter(\$inputs);";
 
     $content = "<?php
 
@@ -84,28 +92,29 @@ if_delete('/%s/*', function ($%s)
 });/*}}}*/";
 
     return sprintf($content, 
+        // if_get all
         $resource_plural,
         $input_content,
         $table,
-
+        // if_put
         $resource_plural,
         $input_content,
         $table, $table,
         $table,
         $table,
-
+        // if_get one
         $resource_plural, $resource_id_key,
         $table, $table, $resource_id_key,
         $table, $table,
         $table,
-
+        // if_post
         $resource_plural, $resource_id_key,
         $table, $table, $resource_id_key,
         $table, $table,
         $input_content,
         $table,
         $table,
-
+        // if_delete
         $resource_plural, $resource_id_key,
         $table, $table, $resource_id_key,
         $table, $table,
@@ -114,7 +123,7 @@ if_delete('/%s/*', function ($%s)
     );
 }/*}}}*/
 
-command('controller:make-restful-from-db', '初始化 controller', function ()
+command('controller:make-restful-from-db', '从数据库表结构初始化 restful 风格 controller', function ()
 {/*{{{*/
     $table = command_paramater('table_name');
 
@@ -146,7 +155,9 @@ command('controller:make-restful-from-db', '初始化 controller', function ()
 
         preg_match('/^`(.*)`/', $line, $matches);
         if ($matches) {
-            $entity_structs[] = $matches[1];
+            $entity_structs[] = [
+                'name' => $matches[1],
+            ];
             continue;
         }
 
@@ -163,5 +174,96 @@ command('controller:make-restful-from-db', '初始化 controller', function ()
     }
 
     error_log(_generate_controller_file($table, $entity_structs, $entity_relationships), 3, $file = CONTROLLER_DIR.'/'.$table.'.php');
+    echo $file."\n";
+});/*}}}*/
+
+command('controller:make-restful-from-description', '从实体描述文件初始化 restful 风格 controller', function ()
+{/*{{{*/
+    $entity_name = command_paramater('entity_name');
+
+    $description = _get_value_from_description_file($entity_name);
+
+    $structs = array_get($description, 'structs', []);
+
+    foreach ($structs as $column => $struct) {
+
+        $tmp = [
+            'name' => $column,
+            'datatype' => $struct['type'],
+            'format' => array_get($struct, 'format', null),
+            'format_description' => array_get($struct, 'format_description', null),
+            'allow_null' => array_get($struct, 'allow_null', false),
+        ];
+
+        if (array_key_exists('default', $struct)) {
+            $tmp['default'] = $struct['default'];
+        }
+
+        $entity_structs[] = $tmp;
+    }
+
+    $relationships = array_get($description, 'relationships', []);
+
+    foreach ($relationships as $relation_name => $relationship) {
+
+        $relation_entity_name = $relationship['entity'];
+        $relation_type = $relationship['type'];
+
+        $entity_relationships[] = [
+            'type' => $relation_type,
+            'relate_to' => $relation_entity_name,
+            'relation_name' => $relation_name,
+        ];
+
+        if ($relation_type !== 'has_many') {
+
+            _get_value_from_description_file($relation_entity_name);
+        }
+    }
+
+    $snaps = array_get($description, 'snaps', []);
+
+    foreach ($snaps as $snap_relation_to_with_dot => $snap) {
+
+        $parent_description = $description;
+
+        $snap_relation_name = '';
+
+        foreach (explode('.', $snap_relation_to_with_dot) as $snap_relation_to) {
+
+            $snap_relation = array_get($parent_description, "relationships.".$snap_relation_to, false);
+
+            otherwise($snap_relation, "与冗余的 $snap_relation_to 没有关联关系");
+            otherwise($snap_relation['type'] !== 'has_many', "冗余的 $snap_relation_to 为 has_many 关系，无法冗余字段");
+
+            $parent_description = _get_value_from_description_file($snap_relation['entity']);
+            $snap_relation_name = $snap_relation_to;
+        }
+
+        $snap_relation_to_structs = $parent_description['structs'];
+
+        foreach ($snap['structs'] as $column) {
+
+            otherwise(array_key_exists($column, $snap_relation_to_structs), "需要冗余的字段 $column 在 $snap_relation_to_with_dot 中不存在");
+
+            $struct = $snap_relation_to_structs[$column];
+
+            $tmp = [
+                'name' => 'snap_'.$snap_relation_name.'_'.$column,
+                'datatype' => $struct['type'],
+                'format' => array_get($struct, 'format', null),
+                'format_description' => array_get($struct, 'format_description', null),
+                'allow_null' => array_get($struct, 'allow_null', false),
+            ];
+
+            if (array_key_exists('default', $struct)) {
+                $tmp['default'] = $struct['default'];
+            }
+
+            $entity_structs[] = $tmp;
+        }
+    }
+
+    error_log(_generate_controller_file($entity_name, $entity_structs, $entity_relationships), 3, $file = CONTROLLER_DIR.'/'.$entity_name.'.php');
     echo $file."\n";
 });/*}}}*/
