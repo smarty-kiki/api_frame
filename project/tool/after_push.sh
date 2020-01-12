8fd672d5a0e40155889ddde24a0ddf62085fb2c5
yaoyang <kiki@smarty.so>
2020-01-12 16:40:16 +0800

    添加 crontab 管理

---
+++ project/tool/after_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../..
+
+ln -fs $ROOT_DIR/project/config/production/nginx/api_frame.conf /etc/nginx/sites-enabled/api_frame
+/usr/sbin/service nginx reload
+
+/bin/bash $ROOT_DIR/project/tool/dep_build.sh link
+/usr/bin/php $ROOT_DIR/public/cli.php migrate:install
+/usr/bin/php $ROOT_DIR/public/cli.php migrate
+
+ln -fs $ROOT_DIR/project/config/production/supervisor/queue_worker.conf /etc/supervisor/conf.d/queue_worker.conf
+/usr/bin/supervisorctl update
+/usr/bin/supervisorctl restart queue_worker:*
+
+ln -fs $ROOT_DIR/project/config/production/crontab/api_frame /etc/cron.d/api_frame
--- project/tool/naming_project.sh
+++ project/tool/naming_project.sh
@@ -14,10 +14,12 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../
 
 mv $ROOT_DIR/project/config/development/nginx/api_frame.conf $ROOT_DIR/project/config/development/nginx/$1.conf
 mv $ROOT_DIR/project/config/production/nginx/api_frame.conf $ROOT_DIR/project/config/production/nginx/$1.conf
+mv $ROOT_DIR/project/config/production/crontab/api_frame $ROOT_DIR/project/config/production/crontab/$1
 
 sed_name $ROOT_DIR/project/config/development/nginx/$1.conf $1
 sed_name $ROOT_DIR/project/config/development/supervisor/queue_worker.conf $1
 sed_name $ROOT_DIR/project/config/production/nginx/$1.conf $1
 sed_name $ROOT_DIR/project/config/production/supervisor/queue_worker.conf $1
+sed_name $ROOT_DIR/project/config/production/crontab/$1 $1
 sed_name $ROOT_DIR/project/tool/start_dev_server.sh $1
 sed_name $ROOT_DIR/project/tool/after_dev_env_start.sh $1
