5c75d9ada6702b3204dcd733643476b7847017a6
yaoyang <kiki@smarty.so>
2020-07-22 22:43:33 +0800

    添加 queue:peek-buried

--- command/queue.php
+++ command/queue.php
@@ -35,3 +35,42 @@ command('queue:pause', '暂停队列任务派发', function ()
 
     sleep($delay);
 });/*}}}*/
+
+command('queue:peek-buried', '处理 buried 状态的任务', function ()
+{/*{{{*/
+    $tube = command_paramater('tube', 'default');
+    $config_key = command_paramater('config_key', 'default');
+
+    $fp = _beanstalk_connection($config_key);
+    _beanstalk_watch($fp, $tube);
+    if ($tube !== 'default') {
+        _beanstalk_ignore($fp, 'default');
+    }
+
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+    while (true) {
+
+        try {
+            $job_instance = _beanstalk_peek_buried($fp);
+            $job_id = $job_instance['id'];
+            $job_instance['body'] = unserialize($job_instance['body']);
+            echo "\033[32m"; var_dump($job_instance); echo "\033[0m";
+
+            $action = command_read('Action', 0, ['kick', 'delete']);
+
+            switch ($action) {
+                case 'kick':
+                    _beanstalk_kick_job($fp, $job_id);
+                    break;
+                case 'delete':
+                    _beanstalk_delete($fp, $job_id);
+                    break;
+            }
+        } catch (throwable $ex) {
+            echo "\033[31m".$ex->getMessage()."\033[0m\n";
+            break;
+        }
+        echo "\n";
+    }
+});/*}}}*/
