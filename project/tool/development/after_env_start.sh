9a8de8d7a3e944a07731020a21559c6a9a02db87
yaoyang <kiki@smarty.so>
2020-12-25 17:41:30 +0800

    配合开发环境的用户密码修改

--- project/tool/development/after_env_start.sh
+++ project/tool/development/after_env_start.sh
@@ -2,5 +2,9 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
+mysql -e "create database \`default\`;\
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';\
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password'"
+
 ENV=development php $ROOT_DIR/public/cli.php migrate:install
 ENV=development php $ROOT_DIR/public/cli.php migrate
